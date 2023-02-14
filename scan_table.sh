316399546f5e1b5c937d92bf93f9d102bb2fa1b8
Jader Dias <52470+JaderDias@users.noreply.github.com>
2023-02-14 15:03:54 +0100

    incorporate megalinter fix suggestions

--- scan_table.sh
+++ scan_table.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if ! grep -F '[profile localhost]' < ~/.aws/config; then
-  echo -e "[profile localhost]\nregion = us-east-1" >> ~/.aws/config
+if ! grep -F '[profile localhost]' <~/.aws/config; then
+	echo -e "[profile localhost]\nregion = us-east-1" >>~/.aws/config
 fi
-if ! grep -F '[localhost]' <  ~/.aws/credentials; then
-  echo -e "[localhost]\naws_access_key_id = NOT_NEEDED\naws_secret_access_key = NOT_NEEDED" >> ~/.aws/credentials
+if ! grep -F '[localhost]' <~/.aws/credentials; then
+	echo -e "[localhost]\naws_access_key_id = NOT_NEEDED\naws_secret_access_key = NOT_NEEDED" >>~/.aws/credentials
 fi
 aws dynamodb scan --table-name table_name --endpoint-url http://localhost:8000 --profile localhost
