924f351afa6316214b9786664fb8931275be5781
JaderDias <52470+JaderDias@users.noreply.github.com>
2023-02-27 22:04:45 +0100

    Remove unused variable "url" in create_user examplefix warning

--- examples/create_user.rs
+++ examples/create_user.rs
@@ -2,7 +2,7 @@
 async fn main() {
     let preferred_username = "test_username";
     let db_client = rust_lambda::dynamodb::get_client().await;
-    if let Ok(url) = std::env::var("LOCAL_DYNAMODB_URL") {
+    if let Ok(_url) = std::env::var("LOCAL_DYNAMODB_URL") {
         rust_lambda::dynamodb::create_table_if_not_exists(&db_client).await;
     }
     let _ = rust_lambda::model::user::create(
