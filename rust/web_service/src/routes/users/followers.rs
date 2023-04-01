cec38fc556a78b897c2c0bb87cb683cbb2a2136a
JaderDias <52470+JaderDias@users.noreply.github.com>
2023-04-01 23:02:37 +0200

    Refactor followers endpoint to return activity+json content-type.

--- rust/web_service/src/routes/users/followers.rs
+++ rust/web_service/src/routes/users/followers.rs
@@ -1,16 +1,20 @@
+use rocket::http::ContentType;
 use rocket::serde::json::Json;
 
+#[derive(rocket::Responder)]
+pub struct Followers(Json<serde_json::Value>, ContentType);
+
 #[rocket::get("/users/<username>/followers")]
-pub fn handler(
-    username: &str,
-    settings: &rocket::State<library::settings::Settings>,
-) -> Json<serde_json::Value> {
+pub fn handler(username: &str, settings: &rocket::State<library::settings::Settings>) -> Followers {
     let user_uri = format!("{}/users/{username}", settings.base_url);
-    Json(serde_json::json!({
+    let body = Json(serde_json::json!({
         "@context": "https://www.w3.org/ns/activitystreams",
         "id": format!("{user_uri}/followers"),
         "type": "OrderedCollection",
         "totalItems": 1, // TODO: real number from db
         "first": format!("{user_uri}/followers?page=1"),
-    }))
+    }));
+    let content_type: ContentType =
+        ContentType::new("application", "activity+json").with_params([("charset", "utf-8")]);
+    Followers(body, content_type)
 }
