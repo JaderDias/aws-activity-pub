75f907cef3f2c01fd527fa9575894ee7b0a68292
JaderDias <52470+JaderDias@users.noreply.github.com>
2023-04-01 22:57:31 +0200

    Webfinger new content type
     "content-type": "application/jrd+json; charset=utf-8"

--- rust/web_service/src/routes/well_known/webfinger.rs
+++ rust/web_service/src/routes/well_known/webfinger.rs
@@ -1,12 +1,16 @@
 use library::model::user;
+use rocket::http::ContentType;
 use rocket::serde::json::Json;
 use tracing::{event, Level};
+
+#[derive(rocket::Responder)]
+pub struct Webfinger(Json<serde_json::Value>, ContentType);
 
 #[rocket::get("/.well-known/webfinger?<resource>")]
 pub async fn handler(
     resource: &str,
     settings: &rocket::State<library::settings::Settings>,
-) -> Option<Json<serde_json::Value>> {
+) -> Option<Webfinger> {
     let split = resource.splitn(2, ':').collect::<Vec<&str>>();
     event!(Level::DEBUG, "{:?}", split);
     if split[0] != "acct" || split.len() < 2 {
@@ -23,14 +27,17 @@ pub async fn handler(
     }
 
     if (user::get(username, settings).await).is_some() {
-        return Some(Json(serde_json::json!({
+        let body = Json(serde_json::json!({
           "subject": resource,
           "links": [{
             "rel": "self",
             "type": "application/activity+json",
             "href": format!("{}/users/{username}", settings.base_url)
           }]
-        })));
+        }));
+        let content_type =
+            ContentType::new("application", "jrd+json").with_params([("charset", "utf-8")]);
+        return Some(Webfinger(body, content_type));
     }
 
     None
