15e0ea75d6d2971fa5ad631210e71fad029eeb5b
JaderDias <52470+JaderDias@users.noreply.github.com>
2023-04-07 21:52:19 +0200

    Refactor deployment process by updating deploy scripts and modifying workflow and file configuration.

--- deploy.sh
+++ deploy.sh
@@ -5,34 +5,6 @@ if [ -z "$S3_BUCKET" ]; then
 	exit 1
 fi
 
-function create_zip() {
-  local dir=$1
-  local zip_file=$2
-  zip -jr "$zip_file" "$dir" \
-    || exit 1
-}
-
-function get_md5sum() {
-  local file=$1
-  if uname -a | grep Darwin; then
-    md5 "$file" | cut -d' ' -f4
-  else
-    md5sum "$file" | cut -d' ' -f1
-  fi
-}
-
-function upload() {
-  local dir=$1
-  local zip_file="$dir.zip"
-  create_zip "$dir" "$zip_file"
-  local md5sum
-  md5sum=$(get_md5sum "$zip_file")
-  aws s3 cp "$zip_file" "s3://$S3_BUCKET/$md5sum" \
-    || exit 1
-  echo "$md5sum"
-}
-
-rm dist/*.zip
 mkdir -p dist/dynamodb_stream
 mkdir -p dist/web_service
 if uname -a | grep x86_64; then
@@ -55,15 +27,12 @@ else
 	cp target/x86_64-unknown-linux-musl/release/web_service dist/web_service/bootstrap
 	cp target/x86_64-unknown-linux-musl/release/dynamodb_stream dist/dynamodb_stream/bootstrap
 fi
-DYNAMODB_STREAM_CODE_OBJECT_KEY=$(upload dist/dynamodb_stream | tail -n1)
-WEB_SERVICE_CODE_OBJECT_KEY=$(upload dist/web_service | tail -n1)
-echo "DYNAMODB_STREAM_CODE_OBJECT_KEY $DYNAMODB_STREAM_CODE_OBJECT_KEY"
-echo "WEB_SERVICE_CODE_OBJECT_KEY $WEB_SERVICE_CODE_OBJECT_KEY"
+aws cloudformation package \
+    --template-file cloudformation.yml \
+    --s3-bucket "$S3_BUCKET" \
+    --output-template-file packaged.yml ||
+    exit 1
 aws cloudformation deploy \
   --capabilities CAPABILITY_IAM \
-  --parameter-overrides \
-    "CodeBucket=$S3_BUCKET" \
-    "DynamodbStreamCodeObjectKey=$DYNAMODB_STREAM_CODE_OBJECT_KEY" \
-    "WebServiceCodeObjectKey=$WEB_SERVICE_CODE_OBJECT_KEY" \
   --stack-name "rust-lambda" \
-  --template-file cloudformation.yml
+  --template-file packaged.yml
